fa1de4ef60b81f68166b768d81692b129040f361
aqmink <89052100421dr@gmail.com>
2025-11-27 18:37:16 +0300

    bearer auth

---
+++ examples/cookie_auth/db.py
@@ -0,0 +1,76 @@
+from typing import Generic
+
+from sqlalchemy import select, update, insert, delete
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from fastapi_auth import ID
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class UserBase(Generic[ID]):
+    __tablename__ = "Users"
+
+    id: Mapped[int] = mapped_column(index=True, unique=True, primary_key=True)
+    username: Mapped[str] = mapped_column()
+    password: Mapped[str] = mapped_column()
+
+
+class User(UserBase, Base):
+    pass
+
+
+class SQLService:
+    def __init__(self, model: Base, session: AsyncSession) -> None:
+        self.model = model
+        self.session = session
+
+    async def get(self, *filter, **params):
+        result = await self.session.execute(
+            select(self.model).
+            filter(*filter).
+            filter_by(**params)
+        )
+        return result.scalars().first()
+
+    async def get_all(
+        self,
+        *filter,
+        limit: int = 0, 
+        offset: int = 50, 
+        **params
+    ):
+        result = await self.session.execute(
+            select(self.model).
+            filter(*filter).
+            filter_by(**params).
+            offset(offset).
+            limit(limit)
+        )
+        return result.scalars().all()
+
+    async def create(self, *args, **data):
+        await self.session.execute(
+            insert(self.model).
+            values(*args, **data)
+        )
+        await self.session.commit()
+
+    async def update(self, *filter, **data):
+        await self.session.execute(
+            update(self.model).
+            filter(*filter).
+            values(**data)
+        )
+        await self.session.commit()
+
+    async def delete(self, *filter, **params):
+        await self.session.execute(
+            delete(self.model).
+            filter(*filter).
+            filter_by(**params)
+        )
+        await self.session.commit()
--- fastapi_auth/__init__.py
+++ fastapi_auth/__init__.py
@@ -3,6 +3,7 @@ from fastapi_auth.backend import Authentication
 from fastapi_auth.models import ID, UP, UserProtocol
 from fastapi_auth.fastapi_auth import FastAPIAuth
 from fastapi_auth.transport.cookie import CookieService
+from fastapi_auth.transport.bearer import BearerService
 from fastapi_auth.token_service.jwt import JWTService
 from fastapi_auth.token_service.db.models import TP, TokenProtocol
 from fastapi_auth.token_service.db.token_service import TokenService
@@ -18,5 +19,6 @@ __all__ = [
     "JWTService",
     "TP",
     "TokenProtocol",
-    "TokenService"
+    "TokenService",
+    "BearerService",
 ]
